58834cdf25e32261f1cad5b164da1f9742b44ee4
nilsmango <simonmilagrolang@icloud.com>
2024-08-16 15:29:56 +0200

    Initial Commit

---
+++ Sound/SoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundApp.swift
+//  Sound
+//
+//  Created by Simon Lang on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
